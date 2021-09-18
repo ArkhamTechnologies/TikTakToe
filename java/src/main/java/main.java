69ff2c7bcfac704e254f9895fe879fcd2a4e284f
sebastian <sebastian.greiner@gmx.net>
2021-09-18 23:19:29 +0200

    initial commit

---
+++ java/src/main/java/main.java
@@ -0,0 +1,31 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        BoardConfiguration b = new BoardConfiguration();
+
+        System.out.println(b + " " + result);
+
+        result =b.add(0, 0);
+
+        System.out.println(b + " " + result);
+
+        result =b.add(0, 1);
+
+        System.out.println(b + " " + result);
+
+        result =b.add(1, 0);
+
+        System.out.println(b + " " + result);
+
+        result =b.add(0, 2);
+
+        System.out.println(b + " " + result);
+
+        result = b.add(2, 0);
+
+        System.out.println(b + " " + result);
+    }
+}
---
+++ java/src/test/java/BoardConfigurationTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class BoardConfigurationTest {
+
+    @Test
+    public void basicTest() {
+
+        BoardConfiguration b = new BoardConfiguration();
+
+
+    }
+
+}
